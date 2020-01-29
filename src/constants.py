66a04fb4712634a5c08e8714d0a6e2f1a92b0541
MLDERES <mlderes@hotmail.com>
2020-01-29 17:04:03 -0600

    update constants.py

--- src/constants.py
+++ src/constants.py
@@ -1,6 +1,6 @@
-<<<<<<< HEAD
 import os
 from pathlib import Path
+import datetime as dt
 
 ROOT = Path(os.getcwd()).parent
 DATA_PATH = ROOT / 'data'
@@ -14,13 +14,7 @@ DS_EXTERNAL = 'external'
 
 FALSE_VALUES = ['No', 'no', 'n', 'N','F','False', 'FALSE']
 TRUE_VALUES = ['Yes', 'yes', 'y', 'Y','T','True','TRUE']
-=======
-import datetime as dt
-from pathlib import Path
 
-ROOT = Path(__file__).parent
-DATA_PATH = ROOT/'data'
-MODEL_PATH = ROOT/'models'
 DS_RAW = DATA_PATH/'raw'
 DS_PROCESSED = DATA_PATH/'processed'
 DS_INTERIM = DATA_PATH/'interim'
@@ -31,4 +25,3 @@ TODAY = dt.datetime.today()
 NOW = dt.datetime.now()
 _DEBUG = False
 DEFAULT_LOGFILE = 'assignment1'
->>>>>>> origin/master
